dd478a6dec780ae7bdaf60e71c1b8dae194eb897
am6DAM2 <am6DAM2@informatica.iesjc>
2021-10-07 08:42:41 +0200

    Initial commit

---
+++ app/src/main/java/com/arias/first/MainActivity.java
@@ -0,0 +1,18 @@
+package com.arias.first;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * @author manu0120sp
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
