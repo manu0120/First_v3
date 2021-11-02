9d64f606a93f94e10714325dded6373f671bed77
am6dam2 <manu0120sp@gmail.com>
2021-11-02 10:17:16 +0100

    Cambiado el metodo onClick por openApp en splash_screen.java. Añadida imagen de fondo en splah_screen y otra animacion (rotate)

--- app/src/main/java/com/arias/first/splash_screen.java
+++ app/src/main/java/com/arias/first/splash_screen.java
@@ -13,6 +13,9 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+
 public class splash_screen extends AppCompatActivity {
 
     @Override
@@ -26,8 +29,18 @@ public class splash_screen extends AppCompatActivity {
         fan.startAnimation(myanim);
 
         //glide for loading girls
+        ImageView mPalmera=findViewById(R.id.palmera);
 
+        Glide.with(this)
+                //load("https://images.unsplash.com/photo-1634733741165-685d61c6a206?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=730&q=80")
+                .load(R.drawable.palmera_fondo)
+                .transition(DrawableTransitionOptions.withCrossFade(2000))
+                .centerCrop()
+                //.circleCrop()
+
+                .into(mPalmera);
     }
+
 
     /*
     public void onClick(View v) {
