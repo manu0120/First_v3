9a39340563fdc47ad1ff1a287e460a5aeb5ff081
am6dam2 <manu0120sp@gmail.com>
2021-11-02 08:50:14 +0100

    Cambiado el metodo onClick por openApp en splash_screen.java

--- app/src/main/java/com/arias/first/splash_screen.java
+++ app/src/main/java/com/arias/first/splash_screen.java
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -17,14 +19,31 @@ public class splash_screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        openApp(true);
 
         ImageView fan = (ImageView) findViewById(R.id.logosplash);
         Animation myanim= AnimationUtils.loadAnimation(this, R.anim.fadein);
         fan.startAnimation(myanim);
+
+        //glide for loading girls
+
     }
 
+    /*
     public void onClick(View v) {
         Intent intent=new Intent (splash_screen.this, LoginActivity.class);
         startActivity(intent);
+    }
+    */
+
+    private void openApp(boolean locationPermission){
+        //Handler: hace q1ue se retrase una accion
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run(){
+                Intent intent = new Intent(splash_screen.this, LoginActivity.class);
+                startActivity(intent);
+            }
+            }, 2000);
     }
 }
