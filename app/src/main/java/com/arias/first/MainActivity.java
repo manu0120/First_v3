49d538b0ecee5fee2cb532871b725d76f0692d41
am6dam2 <manu0120sp@gmail.com>
2021-11-15 14:03:22 +0100

    adicion de nuevo layout (tiene menu contextual) v2

--- app/src/main/java/com/arias/first/MainActivity.java
+++ app/src/main/java/com/arias/first/MainActivity.java
@@ -1,8 +1,18 @@
 package com.arias.first;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,60 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView mycontext= (TextView) findViewById(R.id.textTap);
+        registerForContextMenu(mycontext);
     }
+
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+
+        getMenuInflater().inflate(R.menu.menu_context, menu);
+
+        /*
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_context, menu);
+         */
+    }
+
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.item1:
+                /*
+                Toast toast=Toast.makeText(this, "Item copied", Toast.LENGTH_LONG);
+                toast.show();
+                return true;
+                */
+                /* myMainConstraint: id del layout de activity_main.xml */
+                final ConstraintLayout mLayout =  findViewById(R.id.myMainConstraint);
+
+                Snackbar snackbar = Snackbar
+                        .make(mLayout, "fancy a Snack while you refresh?", Snackbar.LENGTH_LONG)
+
+                        .setAction("UNDO", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                Snackbar snackbar1 = Snackbar.make(mLayout, "Action is restored!", Snackbar.LENGTH_SHORT);
+                                snackbar1.show();
+                            }
+                        });
+
+                snackbar.show();
+
+
+                return true;
+            case R.id.item2:
+                Toast toast2=Toast.makeText(this, "Downloading", Toast.LENGTH_LONG);
+                toast2.show();
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
+
+    }
+
+
 }
