6d8527925da1c43e4ed1244830fe1bb634bc26be
am6dam2 <manu0120sp@gmail.com>
2021-11-19 13:27:14 +0100

    Añadido SwipeRefresh

--- app/src/main/java/com/arias/first/MainActivity.java
+++ app/src/main/java/com/arias/first/MainActivity.java
@@ -3,6 +3,7 @@ package com.arias.first;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
 import android.view.ContextMenu;
@@ -10,22 +11,49 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.webkit.WebView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
-
+    private SwipeRefreshLayout swipeLayout;
+    private WebView miVisorWeb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        /*
         TextView mycontext= (TextView) findViewById(R.id.textTap);
         registerForContextMenu(mycontext);
+        */
+
+        /*
+        cast al layout SwipeRefresh con el que rodeamos la vista en el
+        xml y le colocamos un listener
+         */
+        swipeLayout = findViewById(R.id.myswipe);
+        swipeLayout.setOnRefreshListener(mOnRefreshListener);
+
+        //la vista dentro es un WebView con permiso para zoom
+        miVisorWeb=(WebView) findViewById(R.id.vistaweb);
+        miVisorWeb.getSettings().setBuiltInZoomControls(true);
+        miVisorWeb.loadUrl("https://thispersondoesnotexist.com");
     }
 
+    protected SwipeRefreshLayout.OnRefreshListener
+            mOnRefreshListener = new SwipeRefreshLayout.OnRefreshListener() {
+        @Override
+        public void onRefresh() {
+
+
+            Toast toast0 = Toast.makeText(MainActivity.this, "Hi there! I don't exist :)", Toast.LENGTH_LONG);
+            toast0.show();
+            miVisorWeb.reload();
+            swipeLayout.setRefreshing(false);
+        }
+    };
     public void onCreateContextMenu(ContextMenu menu, View v,
                                     ContextMenu.ContextMenuInfo menuInfo) {
 
